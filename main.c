28d7d8772b35f69723dcf260a05243ac7b926b27
Alex <133706872+atpugh20@users.noreply.github.com>
2024-07-07 22:53:55 -0400

    0.0.3
    - Set read() return time
    - Restructured the main() while loop
    - Turned off output processing
    - Turned off additional flags

--- main.c
+++ main.c
@@ -14,25 +14,29 @@ void disableRawMode() {
 // Gets the terminal's input as raw, flips the bits of raw to 0, then sets the input to raw (0)
 void enableRawMode() {
 	tcgetattr(STDIN_FILENO, &orig_termios); // sets original termios
-	atexit(disableRawMode); // will disable raw on the termination of the program
+	atexit(disableRawMode);
 	struct termios raw = orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // fixes ctrlM, turns off software flow control,
   raw.c_oflag &= ~(OPOST); // turns off output processing
   raw.c_cflag |= (CS8);
-	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // reads bit by bit, sets these to 0: Echo, canonical mode, ctrlV, SIGINT/SIGSTP,
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // reads bit by bit, sets these to 0: Echo, canonical mode, ctrlV, SIGINT/SIGSTP
+  raw.c_cc[VMIN] = 0; // min bytes before read() can return
+  raw.c_cc[VTIME] = 1; // max time to wait before read() returns
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
 	enableRawMode();
-  char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
-    // if c is a control character (ASCII codes 0-31)
-    if(iscntrl(c)) {
+	while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+    if (iscntrl(c)) {
       printf("%d\r\n", c); //%d converts
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
   } 
   return 0;
 }
