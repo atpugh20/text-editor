ceb757acb99a3e76419650548108b3021f67fbbe
Alex <133706872+atpugh20@users.noreply.github.com>
2024-07-10 01:13:03 -0700

    0.0.5

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/*** INCLUDES ***/
+#pragma region /*** INCLUDES ***/
 
 #include <ctype.h>
 #include <errno.h>
@@ -7,27 +7,37 @@
 #include <termios.h>
 #include <unistd.h>
 
-/*** DATA ***/
+#pragma endregion
+#pragma region /***  DEFINES ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+#pragma endregion
+#pragma region /*** DATA ***/
 
 struct termios orig_termios;
 
-/*** TERMINAL ***/
+#pragma endregion
+#pragma region /*** TERMINAL ***/
 
-// Prints an error message and exits the program
 void die(const char *s) {
+  // Prints an error message and exits the program
+
   perror(s);
   exit(1);
 }
 
-// Sets the terminal input back to the original
 void disableRawMode() {
+  // Sets the terminal input back to the original
+
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) 
     die("tcsetattr");
 }
 
-// Gets the terminal's input as raw, flips the bits of raw to 0, then sets the input to raw (0)
 void enableRawMode() {
-	
+	/* Gets the terminal's input as raw, flips the bits 
+     of raw to 0, then sets the input to raw (0)*/
+
   if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 	struct termios raw = orig_termios;
@@ -41,19 +51,47 @@ void enableRawMode() {
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-/*** INIT ***/
+char editorReadKey() {
+  /* waits for one keypress, then returns it */
+
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+#pragma endregion
+#pragma region /*** OUTPUT ***/
+
+void editorRefreshScreen() {
+  
+}
+
+#pragma endregion
+#pragma region /*** INPUT ***/
+
+void editorProcessKeypress() {
+  /* waits for the keypress, then handles it */
+  
+  char c = editorReadKey();
+
+  switch (c) {
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+  }
+}
+#pragma endregion
+#pragma region /*** INIT ***/
 
 int main() {
 	enableRawMode();
-	while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)) {
-      printf("%d\r\n", c); //%d converts
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q') break;
-  } 
+
+  while (1) {
+    editorProcessKeypress();
+  }
+
   return 0;
 }
+#pragma endregion
