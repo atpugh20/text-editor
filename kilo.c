628b407d364ad94ec8507fa86fe2cf745d75de58
Alex <133706872+atpugh20@users.noreply.github.com>
2024-10-19 02:47:59 -0700

    refresh screen and input functions

--- kilo.c
+++ kilo.c
@@ -1,3 +1,12 @@
+/**
+* This program is based on the text-editor tutorial located at:
+* https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html.
+* 
+* This project helped strengthen my skills in the C programming language,
+* terminal manipulation, and memory management. 
+*/
+
+
 /*** INCLUDES ***/
 
 #include <ctype.h>
@@ -7,6 +16,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** DEFINES ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** DATA ***/
 
 struct termios orig_termios;  // Original terminal attributes for exit
@@ -15,8 +28,11 @@ struct termios orig_termios;  // Original terminal attributes for exit
 
 void die(const char* s) {
     /**
-    * Prints an error message and exits the program. Used for error handling.
+    * Used for error handling. Clears the terminal, prints an error message,
+    * and exits the program.
     */
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     perror(s);
     exit(1);
 }
@@ -60,22 +76,63 @@ void enable_raw_mode() {
     }
 }
 
+char editor_read_key() {
+    /**
+    * Waits for one keypress, then returns it.
+    */
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) {
+            die("read");
+        }   
+    }
+    return c;
+}
+
+/*** OUTPUT ***/
+
+void editor_draw_rows() {
+    int y;
+    for (y = 0; y < 24; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+void editor_refresh_screen() {
+    /**
+    * Clears the screen, then draws all the rows.
+    */
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    editor_draw_rows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** INPUT ***/
+
+void editor_process_keypress() {
+    /**
+    * Waits for the keypress, then handles its case. 
+    */
+    char c = editor_read_key();
+    switch (c) {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);    
+            exit(0); 
+            break;
+    }
+}
+
 /*** INIT ***/
 
 int main() {
     enable_raw_mode();
     
     while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-            die("read");
-        }
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q') break;
+        editor_refresh_screen();
+        editor_process_keypress();
     }
 
     return 0;
